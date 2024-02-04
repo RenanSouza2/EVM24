f0bb2a32bad86da59c28902752f1a224fbc48aee
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2024-02-04 11:01:46 -0300

    new lib

--- lib/bytes/test/test.c
+++ lib/bytes/test/test.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-
 #include "../debug.h"
 #include "../../../utils/clu/bin/header.h"
 #include "../../word/debug.h"
---
+++ lib/mem/struct.h
@@ -0,0 +1,6 @@
+#ifndef __EXAMPLE_STRUCT_H__
+#define __EXAMPLE_STRUCT_H__
+
+#include "header.h"
+
+#endif
