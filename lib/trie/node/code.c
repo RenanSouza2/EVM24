46f1160645453b4914fc7119a18bf153beaf79c0
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2024-02-18 21:50:54 -0300

    start node

--- lib/trie/node/code.c
+++ lib/trie/node/code.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 #include "debug.h"
 
 #ifdef DEBUG
--- lib/trie/node/struct.h
+++ lib/trie/node/struct.h
@@ -3,4 +3,21 @@
 
 #include "header.h"
 
+#include "../../utils/struct.h"
+
+#define LEAF        0
+#define BRANCH      1
+#define EXTENSION   2
+
+STRUCT(evm_node)
+{
+    int type;
+};
+
+STRUCT(evm_node_leaf)
+{
+    uchar_p path;
+    uchar_p val;
+};
+
 #endif
