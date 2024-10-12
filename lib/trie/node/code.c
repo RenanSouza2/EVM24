7d805ae8df96ab58c1bd786325da3ce57d770f24
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2024-10-12 11:30:42 -0300

    encode extension

--- lib/trie/node/code.c
+++ lib/trie/node/code.c
@@ -12,6 +12,7 @@
 
 #endif
 
+// TODO test
 evm_rlp_t rlp_init_node_leaf(evm_node_leaf_p n)
 {
     evm_rlp_vec_t r_vec = rlp_vec_init(2);
@@ -20,9 +21,10 @@ evm_rlp_t rlp_init_node_leaf(evm_node_leaf_p n)
     return rlp_init_list(r_vec);
 }
 
+// TODO test
 evm_rlp_t rlp_init_node_branch(evm_node_branch_p n)
 {
-    evm_rlp_vec_t r_vec = rlp_vec_init(2);
+    evm_rlp_vec_t r_vec = rlp_vec_init(17);
     for(uint64_t i=0; i<16; i++)
     {
         byte_vec_t v = byte_vec_init_word(&n->next[i]);
@@ -32,19 +34,33 @@ evm_rlp_t rlp_init_node_branch(evm_node_branch_p n)
     return rlp_init_list(r_vec);
 }
 
+// TODO test
+evm_rlp_t rlp_init_node_extension(evm_node_extension_p n)
+{
+    evm_rlp_vec_t r_vec = rlp_vec_init(2);
+    r_vec.arr[0] = rlp_init_byte_vec(n->path);
+
+    byte_vec_t v = byte_vec_init_word(&n->next);
+    r_vec.arr[1] = rlp_init_byte_vec(v);
+
+    return rlp_init_list(r_vec);
+}
+
+// TODO test
 evm_rlp_t rlp_init_node(evm_node_p n)
 {
     switch (n->type)
     {
         case LEAF: return rlp_init_node_leaf(NL(n));
-        case BRANCH: break;
-        case EXTENSION: break;
+        case BRANCH: return rlp_init_node_branch(NB(n));
+        case EXTENSION: return rlp_init_node_extension(NE(n));
     }
     assert(false);
 }
 
 word_t node_encode_header(evm_node_p n);
 
+// TODO test
 byte_vec_t node_encode(evm_node_p n)
 {
     evm_rlp_t r = rlp_init_node(n);
