2b530c1975146fd58180d71846d6542b67861f7e
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-05-03 15:01:45 -0300

    test gas

--- lib/machine/gas/test/test.c
+++ lib/machine/gas/test/test.c
@@ -1,48 +1,73 @@
 #include "../debug.h"
 #include "../../../../testrc.h"
+#include "../../../../mods/macros/test.h"
 
 #include "../../../utils/debug.h"
 
 
 
-void test_gas_mem()
+void test_gas_mem(bool show)
 {
-    printf("\n\t%s", __func__);
+    TEST_FN_OPEN
 
-    assert_64(gas_mem( 0),  0);
-    assert_64(gas_mem( 1),  3);
-    assert_64(gas_mem(22), 66);
-    assert_64(gas_mem(23), 70);
-    assert_64(gas_mem(32), 98);
-    assert_64(gas_mem(0xffffffffff), U64_MAX);
-    assert_64(gas_mem(     U64_MAX), U64_MAX);
+    #define TEST_GAS_MEM(TAG, SIZE, RES)    \
+    {                                       \
+        TEST_CASE_OPEN(TAG)                 \
+        {                                   \
+            uint64_t res = gas_mem(SIZE);   \
+            assert_64(res, RES);            \
+        }                                   \
+        TEST_CASE_CLOSE                     \
+    }
 
-    assert(clu_mem_is_empty());
+    TEST_GAS_MEM(1,  0,  0);
+    TEST_GAS_MEM(2,  1,  3);
+    TEST_GAS_MEM(3, 22, 66);
+    TEST_GAS_MEM(4, 23, 70);
+    TEST_GAS_MEM(5, 32, 98);
+    TEST_GAS_MEM(6, 0xffffffffff, U64_MAX);
+    TEST_GAS_MEM(7, U64_MAX, U64_MAX);
+
+    #undef TEST_GAS_MEM
+
+    TEST_FN_CLOSE
 }
 
-void test_gas_copy()
+void test_gas_copy(bool show)
 {
-    printf("\n\t%s", __func__);
+    TEST_FN_OPEN
 
-    assert_64(gas_copy( 0), 3);
-    assert_64(gas_copy( 1), 6);
-    assert_64(gas_copy(32), 6);
-    assert_64(gas_copy(64), 9);
-    assert_64(gas_copy(U64_MAX), 0x1800000000000003);
+    #define TEST_GAS_COPY(TAG, SIZE, RES)   \
+    {                                       \
+        TEST_CASE_OPEN(TAG)                 \
+        {                                   \
+            uint64_t res = gas_copy(SIZE);  \
+            assert_64(res, RES);            \
+        }                                   \
+        TEST_CASE_CLOSE                     \
+    }
 
-    assert(clu_mem_is_empty());
+    TEST_GAS_COPY(1,  0, 3);
+    TEST_GAS_COPY(2,  1, 6);
+    TEST_GAS_COPY(3, 32, 6);
+    TEST_GAS_COPY(4, 64, 9);
+    TEST_GAS_COPY(5, U64_MAX, 0x1800000000000003);
+
+    TEST_FN_CLOSE
 }
 
 
 
 void test_gas()
 {
-    printf("\n%s", __func__);
+    TEST_LIB
 
-    test_gas_mem();
-    test_gas_copy();
+    bool show = true;
 
-    assert(clu_mem_is_empty());
+    test_gas_mem(show);
+    test_gas_copy(show);
+
+    TEST_ASSERT_MEM_EMPTY
 }
 
 
